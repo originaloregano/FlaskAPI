6046a5cfa6ca34d023b6b2a40f5b750c1f55741c
originaloregano <aorigitano@gmail.com>
2018-05-24 16:42:05 -0500

    wip

--- app.py
+++ app.py
@@ -1,26 +1,19 @@
 from flask import Flask, render_template
-from data import Articles
+from flask_github import Github
 
 app = Flask(__name__)
-
-Articles = Articles()
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/login')
+def login():
+    return github.authorize()
 
-@app.route('/articles')
-def articles():
-    return render_template('articles.html', articles = Articles)
+app.config['GITHUB_CLIENT_ID'] = 'XXX'
+app.config['GITHUB_CLIENT_SECRET'] = 'YYY'
 
-@app.route('/article/<string:id>/')
-def article(id):
-    return render_template('articles.html', id=id)
-
-
+github = GitHub(app)
 if __name__ == '__main__':
     app.run(debug=True)
