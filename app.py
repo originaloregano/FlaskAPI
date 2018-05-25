9c522f51cb7faf0b9ee3a282300948bf55a76b81
originaloregano <aorigitano@gmail.com>
2018-05-25 15:28:58 -0500

    wip

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from flask import json
+from flask import request
 from flask import Flask, render_template
-from flask_github import Github
+# from flask_github import Github
 
 app = Flask(__name__)
 
@@ -7,13 +9,25 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
-@app.route('/login')
-def login():
-    return github.authorize()
+@app.route('/github', methods=['POST'])
+def api_github_message():
+    if request.headers['Content-Type'] == 'application/json':
+        git_info = json.dumps(request.json)
+        print git_info
+        return json.dumps(request.json)
+# @app.route('/login')
+# def login():
+#     return github.authorize()
 
-app.config['GITHUB_CLIENT_ID'] = 'XXX'
-app.config['GITHUB_CLIENT_SECRET'] = 'YYY'
+@app.errorhandler(404)
+def not_found(e):
+    return 'I dont know what you are talking about, Willis', 404
 
-github = GitHub(app)
+# app.config['GITHUB_CLIENT_ID'] = 'XXX'
+# app.config['GITHUB_CLIENT_SECRET'] = 'YYY'
+
+#setup github-flask
+# github = GitHub(app)
+
 if __name__ == '__main__':
     app.run(debug=True)
