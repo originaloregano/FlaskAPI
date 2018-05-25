1ebec8009fa62246fa99b298b602a456705258e4
originaloregano <aorigitano@gmail.com>
2018-05-25 16:10:29 -0500

    able to communicate with github using ngrok and receive json object

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import json
 from flask import request
 from flask import Flask, render_template
-# from flask_github import Github
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def api_rout():
     return render_template('home.html')
 
 @app.route('/github', methods=['POST'])
@@ -15,19 +14,6 @@ def api_github_message():
         git_info = json.dumps(request.json)
         print git_info
         return json.dumps(request.json)
-# @app.route('/login')
-# def login():
-#     return github.authorize()
-
-@app.errorhandler(404)
-def not_found(e):
-    return 'I dont know what you are talking about, Willis', 404
-
-# app.config['GITHUB_CLIENT_ID'] = 'XXX'
-# app.config['GITHUB_CLIENT_SECRET'] = 'YYY'
-
-#setup github-flask
-# github = GitHub(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
