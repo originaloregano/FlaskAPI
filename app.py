4aa1a8af058220f3b1b3d89513231cea3f164ead
originaloregano <aorigitano@gmail.com>
2018-06-11 20:49:42 -0500

    able to get json in terminal

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import requests
 
 app = Flask(__name__)
 
-url = 'https://api.github.com/users/originaloregano'
-headers = {'user-agent': 'my-app/0.0.1'}
+url = 'https://api.github.com/users/'
+headers = {'key1': 'value1', 'key2': 'value2'}
 r = requests.get(url, headers=headers)
 
 
 # payload = {'key1': 'value1', 'key2': 'value2'}
-r = requests.get('https://api.github.com/events')
+# r = requests.get('https://api.github.com/events')
 
 print(r.status_code)
 print(r.url)
@@ -23,10 +23,10 @@ print(r.json())
 #
 # >>> r = requests.post("http://httpbin.org/post", data=payload)
 # >>> print(r.text)
->>> url = 'https://api.github.com/some/endpoint'
->>> payload = {'some': 'data'}
-
->>> r = requests.post(url, json=payload)
+# >>> url = 'https://api.github.com/some/endpoint'
+# >>> payload = {'some': 'data'}
+#
+# >>> r = requests.post(url, json=payload)
 # >>> r = requests.put('http://httpbin.org/put', data = {'key':'value'})
 # >>> r = requests.delete('http://httpbin.org/delete')
 # >>> r = requests.head('http://httpbin.org/get')
