e4b170336da6220d3be1eaf3bbd53fb7241e7ac4
originaloregano <aorigitano@gmail.com>
2018-06-12 20:14:58 -0500

    clean up

--- app.py
+++ app.py
@@ -13,31 +13,16 @@ def getApi():
 @app.route('/api/<username>', methods=['GET'])
 def getUser(username):
   user_endpoint = api+"/users/"+username
-  user_requests = requests.get(user_endpoint)
-  user_data = user_requests.json()
-  return jsonify(user_data)
-
-@app.route('/api/<username>/<section>', methods=['GET'])
-def getUser(section):
-  user_endpoint = api+"/users/"
-  user_requests = r.get(user_endpoint)
-  user_data = user_requests.json()
-  return jsonify(user_data)
-
-  
-@app.route('/api/<username>', methods=['GET'])
-def getUser(username):
-  user_endpoint = api+"/users/"+username
   user_request = requests.get(user_endpoint)
   user_data = user_request.json()
   return jsonify(user_data)
 
-@app.route('/api/<username>/<section>', methods=['GET'])
-def getUser(section):
-  user_endpoint = api+"/users/{}/{}".format(username,section)
-  user_request = user_request.get(user_endpoint)
-  user_data = user_request.json()
-  return jsonify(user_data)
+# @app.route('/api/<username>/<section>', methods=['GET'])
+# def getUser(section):
+#   user_endpoint = api+"/users/{}/{}".format(username,section)
+#   user_request = user_request.get(user_endpoint)
+#   user_data = user_request.json()
+#   return jsonify(user_data)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
