d47220d64e577bd6607569e66bdec840814bc0af
originaloregano <aorigitano@gmail.com>
2018-06-13 20:54:33 -0500

    display specific data

--- app.py
+++ app.py
@@ -12,11 +12,38 @@ def getApi():
 
 @app.route('/api/<username>', methods=['GET'])
 def getUser(username):
+  """ grab github profile"""
   user_endpoint = api+"/users/"+username
   user_request = requests.get(user_endpoint)
   user_data = user_request.json()
-  return jsonify(user_data)
+  return user_data
 
+
+def manipulateData(user_data):
+    keys = ['followers', 'starred_url', 'public_repos']
+    new_data = {}
+    for key in keys:
+        new_data[key] = user_data[key]
+    return new_data
+
+# @app.route('/display/<display>', methods=['GET'])
+def displayPretty(user_data):
+    """pretty print key and values"""
+    # user_data = getUser(display)
+    variable = ""
+    for key in user_data:
+        variable += "{}: {}".format(key, user_data[key]) + "<br>"
+    return variable
+
+@app.route("/everything/<param>")
+def retrieveAndDisplay(param):
+    user_data = getUser(param)
+    display_data = guts(user_data)
+    return displayKeys(display_data)
+
+
+
+# in order to get more data such as repos from github, need authentication
 # @app.route('/api/<username>/<section>', methods=['GET'])
 # def getUser(section):
 #   user_endpoint = api+"/users/{}/{}".format(username,section)
